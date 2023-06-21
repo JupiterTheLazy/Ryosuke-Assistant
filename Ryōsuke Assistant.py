ccdd7c809dbe7e70c2d77688b434858b2cf0996f
JupiterTheLazy <133185813+JupiterTheLazy@users.noreply.github.com>
2023-06-20 23:27:51 -0300

    Add files via upload

---
+++ Ryōsuke Assistant.py
@@ -0,0 +1,37 @@
+import webbrowser
+import time
+import os
+import playsound
+
+print('Ryōsuke: Hello, Master')
+from playsound import playsound
+playsound('C:\\Users\\Júlia\\Documents\\speak\\Hi master.mp3')
+while True:
+    print('Ryōsuke: How can I help you?')
+    action=input('You: ')
+
+    if action=="gm":
+        print('Ryōsuke: Good Morning!')
+        playsound('C:\\Users\\Júlia\\Documents\\speak\\good-morning.mp3')
+        time.sleep(1)
+        notion = "https://www.notion.so/6780b0c793ea4e0e8c5f14488e213d75"               
+        webbrowser.open(notion)
+    elif action=="study":
+        print('Ryōsuke: Preparing study place...')
+        time.sleep(1)
+        stud = "https://pomofocus.io/app"
+        webbrowser.open(stud)
+        radio = "https://radio.garden/listen/ashiyaradio/agGPOrdP"
+        webbrowser.open(radio)         
+    elif action=="music":
+        print('Ryōsuke: Preparing music...')
+        time.sleep(1)
+        music = "https://youtube.com/playlist?list=PLw4wL1eg6DUxqobY8-9r16ZYg4Dng6I0O"
+        webbrowser.open(music)        
+    elif action=="bye":
+        print('Ryōsuke: See you later.')
+        playsound('C:\\Users\\Júlia\\Documents\\speak\\see you later.mp3')
+        #time.sleep(1)
+        break
+    else:
+        print('Ryōsuke: Invalid input, plase try again')
